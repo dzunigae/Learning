71e95e292478afc11871fe6c068bd3aa15bcc6cc
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2023-02-12 22:07:08 -0500

    Parents and children

--- AngIntro/src/app/app.module.ts
+++ AngIntro/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PostComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
