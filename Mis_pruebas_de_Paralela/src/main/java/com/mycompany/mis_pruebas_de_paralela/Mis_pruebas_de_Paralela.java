48e32b814bcaa9be500781a66b6435e9bf1dc0a5
Daniel Zúñiga <1danielarmando.1999@gmail.com>
2023-04-07 18:16:51 -0500

    Inicio de pruebas de paralela

---
+++ Mis_pruebas_de_Paralela/src/main/java/com/mycompany/mis_pruebas_de_paralela/Mis_pruebas_de_Paralela.java
@@ -0,0 +1,8 @@
+package com.mycompany.mis_pruebas_de_paralela;
+
+public class Mis_pruebas_de_Paralela {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
