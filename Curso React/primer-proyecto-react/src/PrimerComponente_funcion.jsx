757ad04995cf3c48df16f90ab876a80182388815
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2024-03-03 21:03:22 -0500

    Primera sesión React: Introducción y componentes

---
+++ Curso React/primer-proyecto-react/src/PrimerComponente_funcion.jsx
@@ -0,0 +1,6 @@
+//Componente de función atajo rafc
+export const PrimerComponente_funcion = () => {
+  return (
+    <h1>Hola mundo! Estoy aprendiendo React</h1>
+  )
+}
---
+++ Curso React/primer-proyecto-react/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { PrimerComponente_funcion } from "./PrimerComponente_funcion";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PrimerComponente_funcion />
+  </React.StrictMode>
+);
