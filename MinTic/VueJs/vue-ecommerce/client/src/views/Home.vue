7e5493f8f60c648c6bf466895398ec92b575be11
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2026-01-10 09:59:07 -0500

    Tutorial de Vue hasta la diapositiva 211

---
+++ MinTic/VueJs/vue-ecommerce/client/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <BasicLayouts>
+    <h2>Estamos en el HOME</h2>
+  </BasicLayouts>
+</template>
+
+<script>
+  import BasicLayouts from '@/layouts/BasicLayouts.vue';
+  export default {
+    name: 'Home',
+    components: {
+      BasicLayouts,
+    },
+  };
+</script>
---
+++ MinTic/VueJs/vue-ecommerce/client/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <BasicLayouts>
+        <h1>Estamos en Login</h1>
+        <RouterLink to="/register">
+            Crear una cuenta
+        </RouterLink>
+    </BasicLayouts>
+</template>
+
+<script>
+import BasicLayouts from '@/layouts/BasicLayouts.vue';
+
+    export default {
+        name: "Login",
+        components: {
+            BasicLayouts,
+        }
+    }
+</script>
+
+<style>
+
+</style>
