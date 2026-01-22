4f038c1a9b5eafe54e040c5c7e1d3bf6c845092f
Daniel Zuñiga <1danielarmando.1999@gmail.com>
2026-01-22 17:42:51 -0500

    Tutorial React de Mintic truncado, desactualizado

---
+++ MinTic/React/empleados_front/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Form, Button, Container } from "react-bootstrap";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Container>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ MinTic/React/empleados_front/src/components/login/login.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function MiComponente() {
+  const [count, setCount] = useState(0);
+
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+}
